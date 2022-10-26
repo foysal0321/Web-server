f4e016db348071a4181790e0bffc156b20596c0b
Foysal <foysalx4@gmail.com>
2022-10-26 12:44:52 +0600

    data-id route

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ app.use(cors());
 const data = require('./data/data.json')
 
 app.get('/',(req,res)=>{
-    res.send('web is running!')
+    res.send('web is running!') 
 })
 
 app.get('/web',(req,res)=>{
     res.send(data)
+})
+
+app.get('/web/:id',(req,res)=>{
+    const ids= req.params.id;
+    const selectId = data.find(i => i.id === ids);
+    res.send(selectId);
 })
 
 app.listen(port,()=>{
