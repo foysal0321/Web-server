80202482fb90b277a35b789e5e422f834f146655
Foysal <foysalx4@gmail.com>
2022-10-26 12:46:21 +0600

    vercel.json

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-const data = require('./data/data.json')
+const data = require('./data/data.json');
 
 app.get('/',(req,res)=>{
     res.send('web is running!') 
-})
+});
 
 app.get('/web',(req,res)=>{
     res.send(data)
-})
+});
 
 app.get('/web/:id',(req,res)=>{
     const ids= req.params.id;
     const selectId = data.find(i => i.id === ids);
     res.send(selectId);
 })
+
+
 
 app.listen(port,()=>{
     console.log(`web server is running ${port}`);
